6e7d963b771adec0d86e6cea162eecb44645d5a5
patgarcia <commonzenmedia@gmail.com>
2021-09-23 02:23:22 -0400

    Authentication tweaks

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,24 +1,24 @@
-import api from "./apiConfig";
+import api from './apiConfig';
 
-export const loginUser = async (loginData) => {
-  const resp = await api.post("/auth/login", { authentication: loginData });
-  localStorage.setItem("authToken", resp.data.token);
-  api.defaults.headers.common.authorization = `Token ${resp.data.token}`;
+export const loginUser = async loginData => {
+  const resp = await api.post('/dj-rest-auth/login/', loginData);
+  localStorage.setItem('authToken', resp.data.key);
+  api.defaults.headers.common.authorization = `Token ${resp.data.key}`;
   return resp.data.user;
 };
 
-export const registerUser = async (registerData) => {
-  const resp = await api.post("/users/", { user: registerData });
-  localStorage.setItem("authToken", resp.data.token);
+export const registerUser = async registerData => {
+  const resp = await api.post('/users/', { user: registerData });
+  localStorage.setItem('authToken', resp.data.token);
   api.defaults.headers.common.authorization = `Token ${resp.data.token}`;
   return resp.data.user;
 };
 
 export const verifyUser = async () => {
-  const token = localStorage.getItem("authToken");
+  const token = localStorage.getItem('authToken');
   if (token) {
     api.defaults.headers.common.authorization = `Token ${token}`;
-    const resp = await api.get("/auth/verify");
+    const resp = await api.get('/auth/verify');
     return resp.data;
   }
   return null;
