924527d3e67c7d98499d3b511bf2cab0f4facc19
patgarcia <commonzenmedia@gmail.com>
2021-09-23 05:53:12 -0400

    APIURL changed to production server

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = 'https://sleepy-meadow-76573.herokuapp.com/api';
-const baseURL = 'http://localhost:8000/api';
+const baseURL = 'https://sleepy-meadow-76573.herokuapp.com/api';
+// const baseURL = 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: baseURL,
