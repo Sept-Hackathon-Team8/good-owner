f3d1fa4b7f1f3b78b3da47870a50b73805c98940
patgarcia <commonzenmedia@gmail.com>
2021-09-29 20:37:06 -0400

    Recoverred TaskSnippet and Training jsx

--- src/components/training/TaskSnippet.jsx
+++ src/components/training/TaskSnippet.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 const TaskSnippet = ({ task, feedback }) => {
   return (
-    task &&
-    feedback && (
-      <div className="task">
-        {/* TODO: use feedback to create progress circle UI element here */}
-        <div className="task-image">
-          <img alt={task.title} src={task.icon} />
+    <>
+      {task && feedback && (
+        <div className="task">
+          {/* TODO: use feedback to create progress circle UI element here */}
+          <div className="task-image">
+            <img alt={task.title} src={task.icon} />
+          </div>
+          <h3 className="task-text">{task.title}&nbsp;</h3>
+          <p
+            style={{ fontSize: '.8rem', color: '#666' }}
+          >{`gr ${feedback.great} | rf: ${feedback.ruff}`}</p>
         </div>
-        <h3 className="task-text">{task.title}&nbsp;</h3>
-        <p
-          style={{ fontSize: '.8rem', color: '#666' }}
-        >{`gr ${feedback.great} | rf: ${feedback.ruff}`}</p>
-      </div>
-    )
+      )}
+    </>
   );
 };
 export default TaskSnippet;
--- src/components/training/Training.jsx
+++ src/components/training/Training.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState, useContext, useCallback } from 'react';
 import { DoggoContext } from '../../DoggoContext';
 import { Link } from 'react-router-dom';
@@ -78,67 +77,3 @@ const Training = props => {
 };
 
 export default Training;
-=======
-import React from "react";
-import { Link } from "react-router-dom";
-import mark from "../../Images/mark.svg"
-import focus from "../../Images/focus.svg"
-import target from "../../Images/target.svg"
-import sit from "../../Images/sit.svg"
-import ProgressCircle from "../progress-circle/ProgressCircle";
-
-
-const Training = (props) => {
-    return (
-      <div className="training">
-        <div className="unit">
-            <h1> Unit One </h1>
-        </div>
-        <div className="tasks">
-          {/* reusable code, create components */}
-            <div className="task">
-                <div className="task-image">
-                  <ProgressCircle />
-                  {/* <img alt="mark and reward image" src={mark}/> */}
-                </div>
-                <div className="task-text">
-                  Mark and Reward
-                </div>
-            </div>
-            <div className="task">
-                <div className="task-image">
-                {/* <ProgressCircle /> */}
-                  <img alt="auto focus image" src={focus}/>
-                </div>
-                <div className="task-text">
-                  Auto Focus
-                </div>
-            </div>
-            <div className="task">
-                <div className="task-image">
-                  <img alt="hand target image" src={target}/>
-                </div>
-                <div className="task-text">
-                  Hand Target
-                </div>
-            </div>
-            <div className="task">
-                <div className="task-image">
-                  <img alt="sit image" src={sit}/>
-                </div>
-                <div className="task-text">
-                  Sit
-                </div>
-            </div>
-        </div>
-        
-        <Link to="/task">
-          <button className="start-lesson">Start Lesson</button>
-        </Link>
-     
-      </div>
-    );
-  };
-  
-  export default Training;
->>>>>>> main
