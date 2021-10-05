42ddb258894e98c6ec55f64df491efa473a1786b
patgarcia <commonzenmedia@gmail.com>
2021-10-05 18:46:18 -0400

    bugfix: hamburger menu console logs removed

--- src/components/hamburger-menu/HamburgerMenu.jsx
+++ src/components/hamburger-menu/HamburgerMenu.jsx
@@ -1,42 +1,35 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { DoggoContext } from "../../DoggoContext";
-import logo from "../../Images/Doggo.svg";
-import "./HamburgerMenu.css";
-import backarrow from "../../Images/back.svg";
-import { removeToken } from "../../services/auth";
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { DoggoContext } from '../../DoggoContext';
+import logo from '../../Images/Doggo.svg';
+import './HamburgerMenu.css';
+import backarrow from '../../Images/back.svg';
+import { removeToken } from '../../services/auth';
 
-const HamburgerMenu = (props) => {
+const HamburgerMenu = props => {
   const { changeOpenHamburger, openHamburger } = props;
-  console.log(openHamburger);
   const { currentPet, setCurrentPet } = useContext(DoggoContext);
   const { currentUser, setCurrentUser } = useContext(DoggoContext);
-  console.log(currentPet.name);
-  console.log(currentUser);
 
   const handleLogout = () => {
     setCurrentUser(null);
-    localStorage.removeItem("authToken");
+    localStorage.removeItem('authToken');
     removeToken();
-  }
+  };
 
   return (
     <div className="hamburger-menu">
       <img
+        alt="menu"
         className="back"
         src={backarrow}
-        onClick={(e) => changeOpenHamburger(e)}
+        onClick={e => changeOpenHamburger(e)}
       />
       <div className="ham-logo-container">
-        <img
-          className="ham-doggo-logo"
-          alt="doggo logo" src={logo}
-        />
+        <img className="ham-doggo-logo" alt="doggo logo" src={logo} />
         <div className="ham-doggo">DOGGO</div>
       </div>
-      <div className="ham-petname">
-        {currentPet.name}
-      </div>
+      <div className="ham-petname">{currentPet.name}</div>
       <div className="ham-streak-container">
         <div>Streak will go here</div>
       </div>
@@ -47,6 +40,6 @@ const HamburgerMenu = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default HamburgerMenu;
